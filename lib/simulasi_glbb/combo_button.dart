41f3a5311aae9996532c623e20b60923539d93d1
Muhammad Saman <samanmuhammad077@gmail.com>
2023-05-21 14:43:03 +0800

    repair combo button

--- lib/simulasi_glbb/combo_button.dart
+++ lib/simulasi_glbb/combo_button.dart
@@ -1,47 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class ComboButton extends StatelessWidget {
-  const ComboButton({Key? key}) : super(key: key);
-
+  final double x;
+  final double y;
+  final Function(dynamic value) onChangeY;
+  final Function(dynamic value) onChangeX;
+  const ComboButton(
+      {super.key,
+      required this.x,
+      required this.y,
+      required this.onChangeY,
+      required this.onChangeX});
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
+      color: Colors.grey,
       height: 100,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(
-              onPressed: () {}, child: const Icon(Icons.chevron_left)),
+          Container(
+            color: Colors.grey,
+            height: 30,
+            child: Row(
+              children: [
+                const Text('x:'),
+                SfSlider(
+                  min: -640,
+                  max: 640,
+                  value: x,
+                  interval: 320,
+                  showTicks: true,
+                  showLabels: true,
+                  enableTooltip: true,
+                  minorTicksPerInterval: 1,
+                  onChanged: onChangeX,
+                  activeColor: Colors.green,
+                  inactiveColor: Colors.lightGreen,
+                ),
+              ],
+            ),
+          ),
           const SizedBox(
             width: 10.0,
           ),
           ElevatedButton(
               onPressed: () {},
-              child: const Icon(Icons.keyboard_double_arrow_left)),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Icon(
+                Icons.chevron_left,
+                color: Colors.black54,
+              )),
+          const SizedBox(
+            width: 10.0,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Icon(
+                Icons.keyboard_double_arrow_left,
+                color: Colors.black54,
+              )),
           const SizedBox(
             width: 20.0,
           ),
-          Container(
-              color: Colors.grey,
+          SizedBox(
               width: 100,
               height: 30,
               child: TextFormField(
                 controller: TextEditingController(text: ""),
                 style: const TextStyle(color: Colors.black),
-                decoration: const InputDecoration(border: OutlineInputBorder()),
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.greenAccent)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.greenAccent)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black54))),
               )),
           const SizedBox(
             width: 20.0,
           ),
           ElevatedButton(
-              onPressed: () {}, child: const Icon(Icons.chevron_right)),
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Icon(
+                Icons.keyboard_double_arrow_right,
+                color: Colors.black54,
+              )),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Icon(
+                Icons.chevron_right,
+                color: Colors.black54,
+              )),
           const SizedBox(
             width: 10.0,
           ),
-          ElevatedButton(
-              onPressed: () {},
-              child: const Icon(Icons.keyboard_double_arrow_right)),
+          Container(
+            height: 60,
+            color: Colors.grey,
+            padding: const EdgeInsets.only(left: 10),
+            child: Row(
+              children: [
+                const Text('y:'),
+                SfSlider(
+                  min: 0,
+                  max: 480,
+                  value: y,
+                  interval: 140,
+                  showTicks: true,
+                  showLabels: true,
+                  enableTooltip: true,
+                  minorTicksPerInterval: 1,
+                  onChanged: onChangeY,
+                  activeColor: Colors.green,
+                  inactiveColor: Colors.lightGreen,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/simulasi_glbb/main_page.dart
+++ lib/simulasi_glbb/main_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:grafkom_app/simulasi_cermin/components/main_page.dart';
 import 'package:grafkom_app/simulasi_glbb/combo_button.dart';
 import 'package:grafkom_app/simulasi_glbb/init_graph.dart';
-import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class MainPageGLBB extends StatefulWidget {
   const MainPageGLBB({super.key});
@@ -15,11 +14,21 @@ class _MainPageGLBBState extends State<MainPageGLBB> {
   static double x = -640;
   static double y = 480;
   static double diameter = 80;
+  onChangedY(dynamic value) {
+    setState(() {
+      y = value;
+    });
+  }
+
+  onChangeX(dynamic value) {
+    setState(() {
+      x = value;
+    });
+  }
 
   bool isShow = false;
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
       drawer: Drawer(
@@ -62,92 +71,15 @@ class _MainPageGLBBState extends State<MainPageGLBB> {
       body: Column(
         children: [
           InitGraph(x: x, y: y, diameter: diameter),
-          Container(
-            margin: EdgeInsets.only(
-                top: size.height * 3 / 8, left: size.width * 12 / 16 - 100),
-            child: Visibility(
-              visible: isShow,
-              child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: const Color.fromARGB(255, 225, 222, 222),
-                  ),
-                  width: 250,
-                  height: 150,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(children: [
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  const Text('y:'),
-                                  SfSlider(
-                                    min: 0,
-                                    max: 480,
-                                    value: y,
-                                    interval: 140,
-                                    showTicks: true,
-                                    showLabels: true,
-                                    enableTooltip: true,
-                                    minorTicksPerInterval: 1,
-                                    onChanged: (dynamic value) {
-                                      setState(() {
-                                        y = value;
-                                      });
-                                    },
-                                    activeColor: Colors.green,
-                                    inactiveColor: Colors.lightGreen,
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text('x:'),
-                                  SfSlider(
-                                    min: -640,
-                                    max: 640,
-                                    value: x,
-                                    interval: 320,
-                                    showTicks: true,
-                                    showLabels: true,
-                                    enableTooltip: true,
-                                    minorTicksPerInterval: 1,
-                                    onChanged: (dynamic value) {
-                                      setState(() {
-                                        x = value;
-                                      });
-                                    },
-                                    activeColor: Colors.green,
-                                    inactiveColor: Colors.lightGreen,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ])
-                      ],
-                    ),
-                  )),
-            ),
-          ),
           const Spacer(),
-          const ComboButton(),
+          ComboButton(
+            x: x,
+            y: y,
+            onChangeX: onChangeX,
+            onChangeY: onChangedY,
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              isShow = !isShow;
-            });
-          },
-          child: isShow == true
-              ? const Icon(Icons.close)
-              : const Icon(Icons.edit)),
     );
   }
 }
